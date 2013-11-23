c769caa8de64409eab9e4120453dc4902649515e
Benoit Bertholon <benoit.bertholon@uni.lu>
2013-11-23 13:45:05 +0100

    multiple points

--- subtitles_synchronization.py
+++ subtitles_synchronization.py
@@ -23,19 +23,48 @@ def convert_to_s(time):
 
 
 
-def compute_function(t1,rt1,t2,rt2):
+def compute_function_1param(t,rt):
+	b = rt - t
+	return 1, b
+
+def compute_function_2params(t1,rt1,t2,rt2):
 	a = (rt1 -rt2)/(t1-t2)
 	b = rt2 - a * t2
-	def f(t):
-		return a *t + b
-	print a, b
-	return f
+	return a, b
+def f(ranges_and_coeffs,x):
+	for (r, (a,b)) in ranges_and_coeffs:
+		if r[0] <= x <  r[1]:
+			return a*x+b
 
-def syncro_time(f,x):
+def compute_function(points):
+
+	if len(points) < 2:
+		s,v = points[0]
+		coeffs = [((0,float('inf')) ,compute_function_1param(s,v) )]
+	else:
+		coeffs = []
+		j = 0
+		for [p1,p2] in [ points[i:i+2] for i in range(len(points)-1)]:
+			a,b=compute_function_2params(p1[0],p1[1],p2[0],p2[1])
+			print [p1,p2],a,b
+			if j == 0:
+				
+				coeffs.append(((0,p1[0]),(a,b) ))
+			coeffs.append(((p1[0],p2[0]),(a,b) ))
+			if j == len(points)-2:
+				coeffs.append(((p2[0],float("inf")),(a,b) ))
+			j+=1
+	print coeffs
+
+	return coeffs
+
+def syncro_time(coeffs,x,x_list, y_list):
 #	print x.split(":")
 	if len(x.split(":"))==3:
 		t=convert_from_s(x)
-		nt = f(t)
+		x_list.append(t)
+		nt = f(coeffs,t)
+		y_list.append(nt)
 		s=convert_to_s(nt)
 		return s
 	return x
@@ -54,23 +83,44 @@ if __name__ == "__main__":
 
 
 	(option , arg ) = optparser.parse_args(sys.argv)
-	[st1,st2,srt1,srt2] = sys.argv[1:5]
-	inputfile = sys.argv[5]
-	outputfile = sys.argv[6]
-	[t1,t2,rt1,rt2] = map(convert_from_s,[st1,st2,srt1,srt2])
-	print [t1,t2,rt1,rt2]
-	f = compute_function(t1,rt1,t2,rt2)
+	sub = map(convert_from_s,option.sub)
+	vid = map(convert_from_s,option.vid)
+	if len(sub) != len(vid) :
+		print "error not same number of point between subtitle and video"
+
+
+	inputfile = option.inputfile
+	outputfile = option.outputfile
+
+	coeffs = compute_function(zip(sub,vid))
+
 	inp = open(inputfile).read()
 	outp = open(outputfile,'wb')
 
+	x_list = []
+	y_list = []
 	for l in inp.split("\n"):
-		if len(l.split(':')) >= 3:
+		x = l.split(":")
+		if len(x) >= 3:
+
 			ll = l.split()
 #			print ll
-			lll = map (lambda x:syncro_time(f,x),ll)
-#			print " ".join(lll)
+			lll = map (lambda x:syncro_time(coeffs,x,x_list,y_list),ll)
+
+
 			outp.write(" ".join(lll)+"\n")
 		else:
-#			print l
 			outp.write(l+"\n")
+
+	import pylab
+	import numpy
+	
+#	c , a = scipy.optimize.curve_fit(func,list(zip(*points)[0]),list(zip(*points)[1]))
+	pylab.plot(sub,vid,'ro')
+	x__ =numpy.linspace(0,max(x_list),1000)
+	pylab.plot(x_list,y_list,"b.")
+	pylab.plot(x__,x__,"g-")
+	
+	pylab.show()
+
 
